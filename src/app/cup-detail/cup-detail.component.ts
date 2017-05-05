0aa01d5f94a33b1be26748d9baa3e28f602da8f3
tadkim <icanhear@dgu.edu>
2017-05-06 06:05:58 +0900

    feat(bulma): Add Bulma Grid and set scss file to custom files.

--- src/app/cup-detail/cup-detail.component.ts
+++ src/app/cup-detail/cup-detail.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { ViewChild, Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+
+import { AppService } from '../app.service';
+import { Cup } from '../model/cup';
 
 @Component({
-  selector: 'app-cup-detail',
+  selector: 'cup-detail',
   templateUrl: './cup-detail.component.html',
   styleUrls: ['./cup-detail.component.css']
 })
 export class CupDetailComponent implements OnInit {
 
-  constructor() { }
+  
 
+  cup: Cup;
+  imgPath: string = '../../assets/images';
+
+
+  constructor(
+    private appService: AppService,
+    private activatedRoute: ActivatedRoute) { }
+
+  
   ngOnInit() {
+    let cupId: number;
+    this.activatedRoute.params.forEach((urlParameters) => {
+      cupId = parseInt(urlParameters['id']);  
+    });
+
+    this.cup = this.appService.getCup(cupId);
+
+    window.scrollTo(0, 0);                  // 초기화시 최 상단으로 이동
   }
 
+
+
+  onTooltip(act: string) {
+    console.log("tooltip is active!");
+  }
+
+  
 }
